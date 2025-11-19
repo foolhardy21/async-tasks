b4dc2f5f88a6a715596b3d23f9f30e9b241352bb
Vinay Kumar <vinay@Vinsys-MacBook-Pro.local>
2025-11-19 14:54:15 +0530

    feat: pub/sub events for thumbnail task

--- src/services/backgroundTasksCron.ts
+++ src/services/backgroundTasksCron.ts
@@ -7,10 +7,12 @@ import dbInstance from "./database";
 class BackgroundTasksCron {
     #workers: { id: number, occupied: boolean }[]
     #usersInProgress: number[]
+    #eventsMap: Record<string, Function>
 
     constructor() {
         this.#workers = [{ id: 1, occupied: false }, { id: 2, occupied: false }]
         this.#usersInProgress = []
+        this.#eventsMap = {}
     }
 
     start() {
@@ -27,58 +29,88 @@ class BackgroundTasksCron {
                                 },
                                 options: {},
                             }) || []
-                            if (!usersWithNoThumbnails.length) return
                             usersWithNoThumbnails = usersWithNoThumbnails
                                 .filter(user => !this.#usersInProgress.includes(user.id))
+                            if (!usersWithNoThumbnails.length) return
+                            this.subscribeEvent("logs", this.handleLog)
+                            this.subscribeEvent("alert-admin", this.handleAdminAlert)
                             const worker = new Worker("./src/scripts/resizeImage.ts", {
                                 workerData: { workerId: workerObj.id, usersWithNoThumbnails }
                             })
                             this.#usersInProgress.push(...usersWithNoThumbnails.map(user => user.id))
-                            worker.on("message", this.updateWorkertatus.bind(this, workerObj.id, false))
-                            worker.on("error", this.updateWorkertatus.bind(this, workerObj.id, false))
+                            worker.on("message", this.updateWorkerStatus.bind(this, workerObj.id, false))
+                            worker.on("error", this.updateWorkerStatus.bind(this, workerObj.id, false))
                             break
                         default: return
                     }
                     if (task) {
-                        this.updateWorkertatus(workerObj.id, true)
+                        this.updateWorkerStatus(workerObj.id, true)
                     }
                 }
             }
         })
     }
 
-    async updateWorkertatus(workerId: number, newOccupiedStatus: boolean, ...args: any) {
+    async updateWorkerStatus(workerId: number, newOccupiedStatus: boolean, ...args: any[]) {
         this.#workers = this.#workers.map(workerObj => {
             if (workerObj.id === workerId) {
                 return { ...workerObj, occupied: newOccupiedStatus }
             }
             return workerObj
         })
-        if (!args.length) return
-        const { task, thumbnails } = args[0].data
+        if (newOccupiedStatus) return
+
+        const { task } = args[0].data
         switch (task) {
             case "generate-thumbnail":
+                const { thumbnails } = args[0].data
                 const usersCompleted = thumbnails.map((thumbnail: any) => thumbnail.userId)
                 this.#usersInProgress = this.#usersInProgress
                     .filter(userInProgress => !usersCompleted.includes(userInProgress))
-
-                for (const user of thumbnails) {
-                    await dbInstance.update(
-                        {
-                            thumbnailImage: user.path
-                        },
-                        {
-                            where: {
-                                id: {
-                                    [Op.is]: user.userId
+                try {
+                    for (const user of thumbnails) {
+                        await dbInstance.update(
+                            {
+                                thumbnailImage: user.path
+                            },
+                            {
+                                where: {
+                                    id: { [Op.is]: user.userId }
                                 }
                             }
-                        }
-                    )
+                        )
+                    }
+                } catch (err) {
+                    console.log(err)
+                } finally {
+                    this.fireEvent("logs", { data: args[0].data })
+                    this.fireEvent("alert-admin", { data: args[0].data })
                 }
                 break
             default: return
         }
+    }
+
+    subscribeEvent(event: string, handler: Function) {
+        this.#eventsMap = {
+            ...this.#eventsMap,
+            [event]: handler
+        }
+        console.log("subscribed event:", event)
+    }
+
+    fireEvent(event: string, data: Record<string, unknown>) {
+        this.#eventsMap[event].call(this, event, data)
+        delete this.#eventsMap[event]
+        console.log("fired event:", event)
+    }
+
+    handleLog(event: string, data: any) {
+        console.log("logging for", event, data)
+    }
+
+    handleAdminAlert(event: string, data: any) {
+        console.log("sending alert for", event, data)
     }
 
 }
