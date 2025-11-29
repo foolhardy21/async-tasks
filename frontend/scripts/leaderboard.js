35f10406fed8323965b3e6dc9965ed9fe63acc60
Vinay Kumar <vinay@Vinsys-MacBook-Pro.local>
2025-11-29 11:54:56 +0530

    refactor: websockets replaced by sockets.io

--- frontend/scripts/leaderboard.js
+++ frontend/scripts/leaderboard.js
@@ -1,13 +1,13 @@
-const { WebSocket } = require("ws")
+const { io } = require("socket.io-client")
 
 function Leaderboard() {
     this.connectionReady = false
-    this.ws = null
+    this.socket = null
 
     this.makeConnection = function () {
-        const websocket = new WebSocket("ws://localhost:443/ws")
-        this.ws = websocket
-        this.ws.on("message", function (data) {
+        const socket = io("http://localhost:3000", {})
+        this.socket = socket
+        this.socket.on("message", function (data) {
             const { type, payload } = JSON.parse(data)
             switch (type) {
                 case "leaderboard.latest":
@@ -20,12 +20,7 @@ function Leaderboard() {
 
     this.getStandings = function () {
         const data = { type: "leaderboard.latest" }
-        const intervalId = setInterval(() => {
-            if (this.ws.readyState === 1) {
-                this.ws.send(JSON.stringify(data))
-                clearInterval(intervalId)
-            }
-        }, 10)
+        this.socket.send(JSON.stringify(data))
     }
 }
 
--- index.ts
+++ index.ts
@@ -1,34 +1,36 @@
-import WebSocket from "ws"
 import dotenv from "dotenv"
 import express from "express"
+import http from "http"
 import tasksRouter from "./src/routes/tasksRoutes"
 import { EVENTS } from "./src/utils/eventsUtils"
 import userImgServiceInstance from "./src/services/userImage"
 import eventsManager from "./src/services/eventsManager"
 import { GAME_DETAILS, LEADERBOARD_STANDINGS } from "./src/utils/leaderboardUtils"
 import { logger, requestTimer } from "./src/middlewares/common"
+import { Server } from "socket.io"
 
 dotenv.config()
 
 const app = express()
-const wss = new WebSocket.Server({ port: Number(process.env.WS_PORT) })
+const server = http.createServer(app)
+const socketServer = new Server(server)
 
 app.use(express.json())
 app.use(logger)
 app.use(requestTimer)
 app.use("/api/tasks", tasksRouter)
 
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
     console.log(`Server is running at ${process.env.PORT}`)
     eventsManager.subscribe(EVENTS.IMAGE_UPLOAD, userImgServiceInstance.handleImageUpload.bind(userImgServiceInstance))
 })
 
-wss.on("connection", function (ws) {
-    ws.on("message", function (data: string) {
+socketServer.on("connection", function (socket) {
+    socket.on("message", function (data: string) {
         const { type } = JSON.parse(data)
         switch (type) {
             case "leaderboard.latest":
-                ws.send(JSON.stringify({
+                socket.send(JSON.stringify({
                     type,
                     payload: {
                         game: { ...GAME_DETAILS },
